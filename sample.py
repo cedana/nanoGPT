48ee3fff90ffc1e5cbb00410347c00c6e9287d29
Yash Anand <yashanand1910@gmail.com>
2024-07-10 10:22:22 -0400

    Flush print buffer before CR

--- sample.py
+++ sample.py
@@ -82,6 +82,8 @@ if start.startswith('FILE:'):
 start_ids = encode(start)
 x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 
+print("", flush=True)
+
 if wait_for_cr:
     with open(cr_log_file, 'a') as cr_log:
         print('CHECKPOINT', file=cr_log)
